a8580bd230bc238cfd8f5856e9ad04f043634780
maruyi <1135198867@qq.com>
2021-02-08 11:53:31 +0800

    id

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App'
-import { myRequest, baseUrl } from './util/api.js'
+import { myRequest, baseUrl, loginByWeixin } from './util/api.js'
 
 // 将封装的请求接口的方法挂载到全局
 Vue.prototype.$myRequest = myRequest
 
 // 问资源获取url
 Vue.prototype.$baseUrl = baseUrl
+
+Vue.prototype.$login = loginByWeixin
 
 Vue.config.productionTip = false
 
